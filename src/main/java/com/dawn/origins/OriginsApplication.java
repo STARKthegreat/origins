97029fe0f2db93fc3cbd027594b4b6a7a050e6a8
Starkthegreat <EmmanuelKipropKimutai@gmail.com>
2025-02-24 21:04:20 +0300

    feat: update api documentation name

--- src/main/java/com/dawn/origins/OriginsApplication.java
+++ src/main/java/com/dawn/origins/OriginsApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.dawn.origins.repository")
+@OpenAPIDefinition(info = @io.swagger.v3.oas.annotations.info.Info(title = "HNG 12 API Documentation", version = "1.0", description = "HNG 12 STAGE TASKS FOR API Documentation"))
 public class OriginsApplication {
 
 	public static void main(String[] args) {
